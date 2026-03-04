8463e5f41943d5c13a7e537083204ae7c1ad5708
hazozz <hemo0527@outlook.com>
2026-03-05 00:12:05 +0530

    UC2: Added OOPS banner using ASCII characters

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,7 +1,7 @@
-<<<<<<< HEAD
+
 /**
  * OOPSBannerApp - UC2
- * Displays "OOPS" as a large banner using * characters.
+ * This program prints "OOPS" as a banner using * characters.
  * @author Hemanth
  * @version 1.0
  */
@@ -10,18 +10,12 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(" *****    *****    *****   ***** ");
-        System.out.println("*     *  *     *  *     *  *     ");
-        System.out.println("*     *  *     *  *     *  *     ");
-        System.out.println("*     *  *****    *****    ***** ");
-        System.out.println("*     *  *        *              ");
-        System.out.println("*     *  *        *              ");
-        System.out.println(" *****   *        *        ***** ");
-
-=======
-public class OOPSBannerApp {
-    public static void main(String[] args) {
-        System.out.println("OOPS");
->>>>>>> 0658be9792f71907f296995db6e446d73492e66c
+        System.out.println(" *****    *****    *****    ***** ");
+        System.out.println("*     *  *     *  *     *  *      ");
+        System.out.println("*     *  *     *  *     *  *      ");
+        System.out.println("*     *  *     *  *****    *****  ");
+        System.out.println("*     *  *     *  *              *");
+        System.out.println("*     *  *     *  *              *");
+        System.out.println(" *****    *****   *        *****  ");
     }
 }

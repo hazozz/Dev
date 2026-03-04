8aacd52f4fe43aaddc9d3fbd41a20a7387f44ccc
hazozz <hemo0527@outlook.com>
2026-03-04 16:05:13 +0530

    UC2 OOPS Banner App

---
+++ OOPSBannerApp.java
@@ -0,0 +1,21 @@
+/**
+ * OOPSBannerApp - UC2
+ * Displays "OOPS" as a large banner using * characters.
+ * @author Hemanth
+ * @version 1.0
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println(" *****    *****    *****   ***** ");
+        System.out.println("*     *  *     *  *     *  *     ");
+        System.out.println("*     *  *     *  *     *  *     ");
+        System.out.println("*     *  *****    *****    ***** ");
+        System.out.println("*     *  *        *              ");
+        System.out.println("*     *  *        *              ");
+        System.out.println(" *****   *        *        ***** ");
+
+    }
+}

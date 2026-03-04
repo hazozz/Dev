5250a88807e6ace2dfab2cd91fff9a62655ae896
hazozz <hemo0527@outlook.com>
2026-03-05 02:42:57 +0530

    Added UC6 StaticFunctionsBanner files

---
+++ StaticFunctionsBanner.java
@@ -0,0 +1,57 @@
+/**
+ * OOPS Banner App - UC6
+ * Uses static helper methods for O, P, and S characters
+ */
+
+public class StaticFunctionsBanner {
+
+    // Method to generate O pattern
+    static String[] getO() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        };
+    }
+
+    // Method to generate P pattern
+    static String[] getP() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                " ***** ",
+                "*      ",
+                "*      ",
+                "*      "
+        };
+    }
+
+    // Method to generate S pattern
+    static String[] getS() {
+        return new String[]{
+                " ***** ",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "     * ",
+                "     * ",
+                " ***** "
+        };
+    }
+
+    public static void main(String[] args) {
+
+        String[] O = getO();
+        String[] P = getP();
+        String[] S = getS();
+
+        for (int i = 0; i < O.length; i++) {
+            System.out.println(O[i] + "   " + O[i] + "   " + P[i] + "   " + S[i]);
+        }
+    }
+}
